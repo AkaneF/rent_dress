630d068594b0aa97b2dde453a082080a5ba15e93
Ingrid Cheng <bluebear589@gmail.com>
2020-02-24 18:07:22 +0000

    dress mode created,name column added

--- app/controllers/dresses_controller.rb
+++ app/controllers/dresses_controller.rb
@@ -3,6 +3,21 @@ class DressesController < ApplicationController
     @dresses = Dress.all
   end
   def show
-    @dress = Dress.find(param)
+    @dress = Dress.find(params[:id])
+  end
+  def new
+    @dress = Dress.new
+  end
+  def create
+    @dress = Dress.new(dress_params)
+    if @dress
+      @dress.save
+    else
+      render :new
+  end
+
+  private
+  def dress_params
+    params.require(:dress).permit(:name, :price, :category,:size)
   end
 end
