e94a3ced01665f6b77f5120def0f110352e3a638
Ingrid Cheng <bluebear589@gmail.com>
2020-02-25 10:54:39 +0000

    uniqueness in category deleted

--- app/models/dress.rb
+++ app/models/dress.rb
@@ -1,7 +1,7 @@
 class Dress < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :price, presence:true
-  validates :category, presence:true, uniqueness: true
+  validates :category, presence:true
   validates :size, presence: true
   has_one_attached :photo
 end
