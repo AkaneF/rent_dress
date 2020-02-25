0df17af9c920e8c61aa54daf5dab17d04617d487
akane fukui <akane.1418@gmail.com>
2020-02-25 14:46:51 +0000

    adding user_id to review table

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,23 @@
 class BookingsController < ApplicationController
+  def new
+    @dress = Dress.find(params[:dress_id])
+
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @dress = Dress.find(params[:dress_id])
+  end
+
+  def index
+    @bookings = Booking.all
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :confirm)
+  end
+
 end
