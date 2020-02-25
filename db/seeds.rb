bab3a6949d94bfc5b6b67156dde156b017ede5d3
akane fukui <akane.1418@gmail.com>
2020-02-25 17:43:34 +0000

    dressindex_stillinprogress

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,7 @@ require "open-uri"
 
 
 file = URI.open('https://res.cloudinary.com/dhhorrhuq/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1582645769/5lcbmqy0sl8sy8nfss5ok161me12.jpg')
+file_one = URI.open('https://res.cloudinary.com/dhhorrhuq/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1582645769/5lcbmqy0sl8sy8nfss5ok161me12.jpg')
 
 
 User.create email: "test@test.com", password: "123456"
@@ -28,18 +29,21 @@ dress_one = Dress.create(
 
 dress_one.photo.attach(io: file, filename: "test.jpg" , content_type: 'image/png')
 
-Dress.create(
+dress_two = Dress.create(
   price: 1000,
   name: "FLOUR FLAVOR",
   category: "Prom dress",
   size: "uk 8"
   )
-Dress.create(
+dress_two.photo.attach(io: file, filename: "test.jpg" , content_type: 'image/png')
+
+dress_three = Dress.create(
   price: 2000,
   name: "BCBG MAX AZRIA",
   category: "Wedding guest dress",
   size: "uk 10"
   )
+dress_three.photo.attach(io: file, filename: "test.jpg" , content_type: 'image/png')
 
 Review.create(
   content: "Very good quality",
