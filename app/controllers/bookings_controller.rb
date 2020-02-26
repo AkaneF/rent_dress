455a2f2663f85b632be9401509a84b0f5f8249dd
Ingrid Cheng <bluebear589@gmail.com>
2020-02-26 00:35:32 +0000

    Booking Controller,View, Routes completed with index, new, create, destroy function

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,18 +1,32 @@
 class BookingsController < ApplicationController
-  def new
-    @dress = Dress.find(params[:dress_id])
-
-    @booking = Booking.new
-  end
-
-  def create
-    @booking = Booking.new(booking_params)
-    @dress = Dress.find(params[:dress_id])
-  end
 
   def index
     @bookings = Booking.all
   end
+
+  def new
+    @dress = Dress.find(params[:dress_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @dress = Dress.find(params[:dress_id])
+    @booking = Booking.new(booking_params)
+    @booking.dress = @dress
+    @booking.user = current_user
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path
+  end
+
 
   private
 
