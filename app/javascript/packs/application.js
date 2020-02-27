aff5f7f3413c33df3536724379f5a0d56ac22343
Ingrid Cheng <bluebear589@gmail.com>
2020-02-27 17:07:41 +0000

    geocoder added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,3 +22,8 @@ require("channels")
 // WRITE YOUR OWN JS STARTING FROM HERE 👇
 // ----------------------------------------------------
 import "bootstrap";
+import { initMapbox } from './init_mapbox';
+
+document.addEventListener('turbolinks:load', () => {
+  initMapbox();
+})
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,4 @@ class Booking < ApplicationRecord
   belongs_to :user
   validates :start_date, presence: true
   validates :end_date,presence: true
-  validates :confirm, presence:true
 end
